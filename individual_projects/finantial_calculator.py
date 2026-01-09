c7e11fc224559a2c8d8dd6da7c3cda86670fbfea
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-01-09 09:45:01 -0700

    1/9/26

--- individual_projects/finantial_calculator.py
+++ individual_projects/finantial_calculator.py
@@ -2,17 +2,38 @@
 
 #Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
 
+#enter what your job is and how much you are getting payed per month/weekly
+
+#starting menu (menu)
+# enter a number to select an optoin
+# 1 saving time calculator ()
+# 2 compound interest calculator ()
+# 3 budget allocator ()
+# 4 sale price calculator (if an item is on sale divide it by the percent)
+# 5 tip clculator (adds how much you have earn from tips/day) 
+
+# if menu = 1
+    # enter a savings goal how much are you trying to save (save)
+    # how often are you saving?
+        # 1 weekly
+        # 2 monthly
+        # enter how much are you contributing?
+        # if save is 1
+            #display 
+
+#enter a number
+#1 for an item that you want and how much it will cost
+#2 for how much you are paying for housing
+#3 for if you have a car and how much gas cost you
+#4 for what you want and how much grocries will cost you
+#5 for if you get tips and how much each day you have gotten
+#6 for whatever you might have missed 
+
 
 #How long it will take to save for a goal based on a weekly or monthly deposit
-save = 0
-thing = 0
-rent = 10
-car = 10
-grocries = 5
 
 #Compound Interest Calculator
 
 #Sale Price Calculator (apply discounts to prices)
 #Tip Calculator
 
-#hi
