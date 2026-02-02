e2ea0d7c01008ddc6f6c3941c7fbde70c5611a51
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-02-02 09:39:58 -0700

    asdf

--- Notes/reading_files.py
+++ Notes/reading_files.py
@@ -1,13 +1,16 @@
 #BG 1st reading files
+import csv
 while True:
     try:
         #How do you open a file in your program?
         #Notes/reading.txt
         with open("Notes\\reading.txt", "r") as file:
-    
+
         #How do you alter text to work as data in a program?
-            content = file.read()
-            print(content)
+            for line in file:
+                print(f"Hello{line.strip()}")
+            #content = file.read()
+            #print(content)
 
 #How do you keep your program from breaking if the file can't be found?
 #try
@@ -16,10 +19,12 @@ while True:
         print("that file can't be found")
     else:
         print("My code works!")
-        print(content)
         break
 #What is a CSV file Download CSV file?
-
+try:
+    with open("C:\Users\brett.gerlach\Downloads\Class CSV sample - Sheet1.csv", mode = "r") as File:
+    #CSV = comma Seporated Values files
+    
 #How are they used in programming?
 
 #How id reading a CSV different from reading a txt file?
