77c12af1b015e8de4d64c1271a12077c0bd10bf9
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-02-11 09:54:13 -0700

    asdf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- individual_projects/personal_library_updates/personal_library_update.py
+++ individual_projects/personal_library_updates/personal_library_update.py
@@ -1,2 +1,15 @@
 #BG 1st update personal library
 
+#libraries for importing/exporting csv data
+
+#funtion for searching in csv file as "csvff()"
+    #display "1 to add onto the csv file\n2 to clear and write on the csv file\n3 to read on the csv file\n4 to read and write on the csv file\n5 to go back to main menu"
+    #user enters a number of what they want to do
+    #if user entered 1
+        #display and ask "You want to put add something but it must be in a "
+    #else if user entered 2
+    #else if user entered 3
+    #else if user entered 4
+    #else if user entered 5
+#funtion for searching in text file
+#funtion for main
