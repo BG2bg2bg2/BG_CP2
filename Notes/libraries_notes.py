e08f2bddc3903f74b8814202a667dc8834004cea
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-03-30 09:42:54 -0600

    lib notes done

--- Notes/libraries_notes.py
+++ Notes/libraries_notes.py
@@ -3,3 +3,36 @@
 #Why do we import other libraries? 
 #What information do you find in the documentation for a library?
 #What are good sources for tutorials on a library you have never used before?
+
+import numpy as np
+a = np.arange(15).reshape(3,5)
+print(a.shape)
+print(a.ndim)
+print(a.dtype.name)
+print(a.itemsize)
+print(type(a))
+b = np.array([6,7,8])
+print(type(b))
+
+import pandas as pan
+
+from faker import Faker
+fake = Faker()
+
+fake.name()
+# 'Lucy Cechtelar'
+
+fake.address()
+# '426 Jordy Lodge
+#  Cartwrightshire, SC 88120-6700'
+
+fake.text()
+# 'Sint velit eveniet. Rerum atque repellat voluptatem quia rerum. Numquam excepturi
+#  beatae sint laudantium consequatur. Magni occaecati itaque sint et sit tempore. Nesciunt
+#  amet quidem. Iusto deleniti cum autem ad quia aperiam.
+#  A consectetur quos aliquam. In iste aliquid et aut similique suscipit. Consequatur qui
+#  quaerat iste minus hic expedita. Consequuntur error magni et laboriosam. Aut aspernatur
+#  voluptatem sit aliquam. Dolores voluptatum est.
+#  Aut molestias et maxime. Fugit autem facilis quos vero. Eius quibusdam possimus est.
+#  Ea quaerat et quisquam. Deleniti sunt quam. Adipisci consequatur id in occaecati.
+#  Et sint et. Ut ducimus quod nemo ab voluptatum.'
