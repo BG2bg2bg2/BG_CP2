b399019741171752f5c84d7400343fbf04a264dd
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-03-30 09:20:01 -0600

    hi

---
+++ Notes/libraries_notes.py
@@ -0,0 +1,5 @@
+#What is a library? 
+#What are some standard python libraries?
+#Why do we import other libraries? 
+#What information do you find in the documentation for a library?
+#What are good sources for tutorials on a library you have never used before?
