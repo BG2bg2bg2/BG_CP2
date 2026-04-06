87d20e719ca71d181c53da9cf7711eca501fa49a
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-04-06 09:51:49 -0600

    gui note

---
+++ Notes/Gui_notes.py
@@ -0,0 +1,38 @@
+#What is a GUI?
+    # Graphic User Interface
+#What is a widget?
+    #
+#How do I set up a basic GUI?
+#How do I make a clickable button?
+#How do you show and hide widgets? 
+
+#from img import *
+import tkinter as tk
+#base for the first line of the widget
+root = tk.Tk()
+
+
+
+root.title("Testing")
+root.configure(background="orange")
+root.minsize(250, 250)
+root.maxsize(1000,1000)
+#set the start size
+root.geometry("300x300+700+100")
+label = tk.Label(root, text="This is curently working!", font=("Times New Roman", 14, "bold"))
+label.config(fg ="blue", background = "orange")
+
+#image = tk.PhotoImage(file = "img\\image.png")
+#tk.Label(root, image = image)
+
+#stuff about button
+root.count = 0
+def add():
+    root.count += 1
+    tk.Label(root, text = root.count).pack()
+
+btn = tk.Button(root, text="ADD", command=add)
+btn.pack()
+
+label.pack()
+root.mainloop()
