8938791f778361abb63041a87ead82657679aca8
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-04-14 09:53:47 -0600

    just started the gui portfolio-

---
+++ individual_projects/personal_portfolio.py/gui.py
@@ -0,0 +1,9 @@
+#import pygame
+import pygame as p
+import tkinter as tk
+
+def display():
+    print("Finantial calculator allows you to know what your finance is at")
+    print("Morse code translator allows you to read and wright words into morsecoe")
+    print("Fractal pattern genorator makes diferent size shapes fit into that same shape but smaller")
+    print("Random password genorator makes passwords into more complesx passwords that you can use.")
